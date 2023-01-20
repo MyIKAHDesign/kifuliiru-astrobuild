964a419f5b7439af233defbf84de8fee47e61c72
MyIKAHDesign <99662370+MyIKAHDesign@users.noreply.github.com>
2023-01-20 13:36:21 +0000

    My commit after adding vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,18 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import image from "@astrojs/image";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   // Astro uses this full URL to generate your sitemap and canonical URLs in your final build
-  site: "https://astrowind.vercel.app/",
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    sitemap({}),
-    image(),
-  ],
+  site: "https://kifuliiru.vercel.app/",
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), sitemap({}), image()],
+  adapter: vercel()
 });
